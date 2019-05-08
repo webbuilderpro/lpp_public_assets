022df40624dc2568563648bbeb026ab2d4167ede
Akash Anand <akash.anand708@gmail.com>
2019-05-08 15:55:57 +0530

    LPP-266 Change image url

--- onix/scripts.js
+++ onix/scripts.js
@@ -221,19 +221,5 @@ jQuery(function ($) {
 
     });
 
-    //  10. product details popup
-    $('.popup-details').magnificPopup({
-        type: 'inline',
-        fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
-        fixedBgPos: true,
-        overflowY: 'auto',
-        closeBtnInside: true,
-        preloader: false,
-        midClick: true,
-        removalDelay: 300,
-        mainClass: 'my-mfp-slide-bottom'
-        
-    });
-
 
 }); // JQuery end
